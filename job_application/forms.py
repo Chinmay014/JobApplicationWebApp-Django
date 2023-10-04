1d9d9c9cb38e1d7bc31d7e71632edd010de865a3
Chinmay014 <chinmays117@gmail.com>
2023-10-05 00:24:05 +0200

    connect view and database

--- job_application/forms.py
+++ job_application/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 class ApplicationForm(forms.Form):
     first_name = forms.CharField(max_length=80)
+    print(first_name)
     last_name = forms.CharField(max_length=80)
     email = forms.EmailField()
     date = forms.DateField()
